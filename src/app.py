6c547f37f6a08c04fb8180580ba8f140cc27843a
AndresBuendia <124879523+AndresBuendia@users.noreply.github.com>
2024-05-01 11:38:25 +0000

    corrección mapeo inverso

--- src/app.py
+++ src/app.py
@@ -5,6 +5,9 @@ import joblib
 app = Flask(__name__)
 model = joblib.load('../models/wine_quality_model.pkl')
 
+# Mapeo inverso de etiquetas definido en tu exploración de datos
+inverse_label_mapping = {0: 4, 1: 5, 2: 6, 3: 7, 4: 8}
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -15,7 +18,8 @@ def predict():
     final_features = [np.array(features)]
     prediction = model.predict(final_features)
     
-    output = round(prediction[0], 2)
+    # Aplicar el mapeo inverso para convertir la predicción a la escala original
+    output = inverse_label_mapping[prediction[0]]
     
     return render_template('index.html', prediction_text='Predicted Wine Quality: {}'.format(output))
 
