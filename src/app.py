692194e465e10810d599140fd488c774ed76bf80
AndresBuendia <124879523+AndresBuendia@users.noreply.github.com>
2024-05-01 11:26:08 +0000

    FLASK

--- src/app.py
+++ src/app.py
@@ -1,4 +1,23 @@
-from utils import db_connect
-engine = db_connect()
+from flask import Flask, request, render_template
+import numpy as np
+import joblib
 
-# your code here
+app = Flask(__name__)
+model = joblib.load('../models/wine_quality_model.pkl')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = [float(x) for x in request.form.values()]
+    final_features = [np.array(features)]
+    prediction = model.predict(final_features)
+    
+    output = round(prediction[0], 2)
+    
+    return render_template('index.html', prediction_text='Predicted Wine Quality: {}'.format(output))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)
